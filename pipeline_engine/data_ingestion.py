cf70ea172d5ce1e732cefd3b491750ff6b5d5e04
luizmatoss <65014388+luizmatoss@users.noreply.github.com>
2025-03-26 13:12:58 +0000

    Update .gitignore and modify main.py and data_ingestion.py

--- pipeline_engine/data_ingestion.py
+++ pipeline_engine/data_ingestion.py
@@ -1,5 +1,5 @@
-from pyspark.sql import SparkSession
 import logging
+import os
 
 class DataIngestion:
     def __init__(self, spark, bronze_path):
@@ -10,8 +10,12 @@ class DataIngestion:
     def import_data(self):
         try:
             self.logger.info("Starting data import...")
-            green_df = self.spark.read.option("header", "true").csv(f"{self.bronze_path}/green_tripdata_2021_01.csv")
-            yellow_df = self.spark.read.option("header", "true").csv(f"{self.bronze_path}/yellow_tripdata_2021_01.csv")
+            green_file_path = os.path.join(self.bronze_path, "green_tripdata_2021_01.csv")
+            yellow_file_path = os.path.join(self.bronze_path, "yellow_tripdata_2021_01.csv")
+
+            green_df = self.spark.read.option("header", "true").csv(green_file_path)
+            yellow_df = self.spark.read.option("header", "true").csv(yellow_file_path)
+
             self.logger.info(f"Loaded Green dataset with {green_df.count()} records.")
             self.logger.info(f"Loaded Yellow dataset with {yellow_df.count()} records.")
             return green_df, yellow_df
